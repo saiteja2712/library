5d8b89870bbb395a926774e0bf84d6cb00c1dec0
saiteja2712 <143396882+saiteja2712@users.noreply.github.com>
2024-05-07 09:57:42 +0530

    Delete Librarym directory

--- Librarym/src/main/java/com/library/Roles/Role.java
+++
@@ -1,8 +0,0 @@
-package com.library.Roles;
-
-public enum Role {
-	ROLE_USER,
-	ROLE_ADMIN,
-	ROLE_POWERADMIN
-
-}
--- Librarym/src/main/java/com/library/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package com.library.config;
-
-public class JwtConstant {
-	public static String JWT_HEADER="Authorization";
-	public static String SECRET_KEY="fhdhgffghfghfhjutuyedfjgvmbv!ghfuyfjhghfhjfjhfjfhvhyighjgjhjhgjghjgjhfjhfjhvjhvjhgig";
-
-}
